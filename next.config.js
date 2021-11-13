87c3152c99fea90922309235e000716ae1c20c82
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2021-11-13 00:20:40 -0700

    fix: tailwind build issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/onboardscan',
+        destination: '/?fromscan=1&ref=onboarding-qr',
+        permanent: false,
+      },
+    ]
+  },
 }
